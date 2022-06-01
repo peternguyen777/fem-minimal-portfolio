a0a8f05534c13d6d52b38ab89eddbe410d9f21b0
peternguyen777 <peter.quang.nguyen@gmail.com>
2022-06-01 19:26:27 +1000

    portfolio

---
+++ components/PortfolioProjects.js
@@ -0,0 +1,28 @@
+const PortfolioProjects = [
+  {
+    imageMobile: "/portfolio/mobile/image-portfolio-manage@2x.jpg",
+    title: "Manage",
+    introduction:
+      "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.",
+  },
+  {
+    imageMobile: "/portfolio/mobile/image-portfolio-bookmark@2x.jpg",
+    title: "Bookmark",
+    introduction:
+      "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
+  },
+  {
+    imageMobile: "/portfolio/mobile/image-portfolio-insure@2x.jpg",
+    title: "Insure",
+    introduction:
+      "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation.",
+  },
+  {
+    imageMobile: "/portfolio/mobile/image-portfolio-fylo@2x.jpg",
+    title: "Fylo",
+    introduction:
+      "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.",
+  },
+];
+
+export default PortfolioProjects;
---
+++ pages/portfolio/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import PortfolioProjects from "../../components/PortfolioProjects";
+import ButtonSecondary from "../../components/ButtonSecondary";
+
+const index = () => {
+  return (
+    <div>
+      <Header />
+
+      <main className='mt-[104px] bg-secondary_grey_bg px-8'>
+        {PortfolioProjects.map((item) => {
+          return (
+            <div key={item.title}>
+              <img src={item.imageMobile} />
+              <hr className='mt-8 mb-6' />
+              <h2 className='mb-6'>{item.title}</h2>
+              <p className='mb-6 text-[15px]'>{item.introduction}</p>
+              <ButtonSecondary>VIEW PROJECT</ButtonSecondary>
+              <hr className='mt-6 mb-[72px]' />
+            </div>
+          );
+        })}
+        <div className='flex flex-col items-center'>
+          <h2 className='mt-2 mb-10 text-center'>
+            Interested in doing a project together?
+          </h2>
+          <ButtonSecondary>CONTACT ME</ButtonSecondary>
+          <div className='mt-[50px]' />
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default index;
