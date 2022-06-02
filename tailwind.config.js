906819fa6b683b5cfcfc3dcd2dd284b7651329d1
peternguyen777 <peter.quang.nguyen@gmail.com>
2022-06-03 00:50:10 +1000

    tablet index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,13 +20,8 @@ module.exports = {
     },
     screens: {
       sm: "640px",
-      // => @media (min-width: 576px) { ... }
-
       md: "768px",
-      // => @media (min-width: 960px) { ... }
-
       lg: "1440px",
-      // => @media (min-width: 1440px) { ... }
     },
   },
   plugins: [],
